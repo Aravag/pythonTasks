ae5d15a943de2cf263ce058c1a0455e38bf53435
Vagan Arakelyan <vra.vagan@gmail.com>
2023-02-02 12:42:36 +0530

    task 14 is done

---
+++ task14.py
@@ -0,0 +1,13 @@
+# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), 
+# не превосходящие числа N.
+num = int(input('Введите число: '))
+numList = []
+i = 0
+sqrtNum = 0
+while sqrtNum < num:
+    sqrtNum = 2 ** i
+    numList.append(sqrtNum)
+    i += 1 
+if numList[-1] > num:
+    numList.pop()
+print(numList)
