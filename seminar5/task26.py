cef2d37312427a50c48fd4a48dda4d7cd76c4252
Vagan Arakelyan <vra.vagan@gmail.com>
2023-02-13 14:29:05 +0530

    Task 26 is done

---
+++ seminar5/task26.py
@@ -0,0 +1,8 @@
+def exponentiation(a, b):
+    if (b == 1):
+        return a
+    if (b != 1):
+        return (a * exponentiation(a, b - 1))
+a = int(input("Введите число: "))
+b = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", exponentiation(a, b))
